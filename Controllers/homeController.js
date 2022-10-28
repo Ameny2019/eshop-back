0ddb23ea436f47d7f0786ca4f4ec432290cb6657
dagbouj hatem <dagboujhatem@gmail.com>
2022-10-28 23:01:52 +0100

    create product details

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -2,8 +2,35 @@ const Product = require("../Models/product");
 
 exports.getProducts = async (req, res) => {
     try {
-        const products = await Product.find();
+        const products = await Product.find().sort({_id:"desc"})
+        .populate({
+            path: "estamp",
+            model: "estamps"
+        }).populate({
+            path: "efleur",
+            model: "efleur"
+        });
         return res.status(200).json(products);
+    } catch (error) {
+        return res.status(404).json({ message: error.message });
+    }
+}
+
+exports.getProductDetails = async (req, res) => {
+    try {
+        const productFound = await Product.findById(req.params.id)
+        .populate({
+            path: "estamp",
+            model: "estamps"
+        }).populate({
+            path: "efleur",
+            model: "efleur"
+        });
+        if (productFound === null) {
+            res.status(404).json({ message: "Produit non trouvé!" });
+        } else {
+            return res.status(200).json(productFound);
+        }
     } catch (error) {
         return res.status(404).json({ message: error.message });
     }
--- Routers/homeRouter.js
+++ Routers/homeRouter.js
@@ -2,5 +2,6 @@ const routeHome = require("express").Router();
 const homeController = require("../Controllers/homeController");
 
 routeHome.get("/products", homeController.getProducts);
+routeHome.get("/products/:id", homeController.getProductDetails);
 
 module.exports = routeHome;
