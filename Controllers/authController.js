e508337b3cc75584fc77e5e40becf167d971c518
dagbouj hatem <dagboujhatem@gmail.com>
2022-10-28 02:05:43 +0100

    add profile route

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -4,7 +4,6 @@ const { randomBytes } = require("crypto");
 const jwt = require("jsonwebtoken");
 const sendEmail = require('../Utils/mail');
 
-
 const register = async (req, res) => {
   try {
     const userFound = await User.findOne({
@@ -111,153 +110,20 @@ const logout = async (req, res) => {
     console.log(error);
     res.status(500).json({ message: "Erreur interne dans le serveur!" });
   }
-};
-
-module.exports = {
-  register,
-  activationAccount,
-  login,
-  logout,
 }
-// // const transporter = nodemailer.createTransport({
-// //   service: "gmail",
-// //   auth: {
-// //     user: "rimtest43@gmail.com",
-// //     pass: "rim44test**",
-// //   },
-// // });
 
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: "testameny92@gmail.com",
-//     pass: "Testameny2021",
-//   },
-// });
+const profile = async (req, res) => {
+  try {
+    return res.status(200).json({ data: req.user });
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+}
 
-// function sendEmail(email, token) {
-//   const mail = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//       user: 'ameny.ouelhazi@gmail.com', // Your email id
-//       pass: 'sticl1e2009' // Your password
-//     }
-//   });
-//   const mailOptions = {
-//     from: 'tutsmake@gmail.com',
-//     to: email,
-//     subject: 'Reset Password Link - Tutsmake.com',
-//     html: '<p>You requested for reset password, kindly use this <a href="http://localhost:3000/reset-password?token=' + token + '">link</a> to reset your password</p>'
-//   };
-//   mail.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//       console.log(1)
-//     } else {
-//       console.log(0)
-//     }
-//   });
-// }
-
-// module.exports = {
-//   verifyEmail: async function (req, res) {
-//     try {
-//       // let verificationCode = req.params.verificationCode;
-//       const { verificationCode } = req.params;
-
-//       const user = await User.findOne({
-//         verificationCode,
-//       });
-//       // console.log(user.fullname);
-//       if (!user) {
-//         return res.status(401).json({
-//           status: 401,
-//           message: "Unauthorized access, Invalid verification code",
-//         });
-//       }
-//       user.verified = true;
-//       user.verificationCode = undefined;
-//       user.save();
-//       return res.sendFile(
-//         join(__dirname, "../Templates/verification_success.html")
-//       );
-//     } catch (error) {
-//       res.sendFile(join(__dirname, "../Templates/errors.html"));
-//     }
-//   },
-
-//   profile: async function (req, res) {
-//     // return res.status(404).json({ message: "this my profile" });
-//     try {
-//       const user = req.user;
-//       return res.status(200).json({ data: user });
-//     } catch (error) {
-//       return res.status(404).json({ message: error.message });
-//     }
-//   },
-
-//   //************************************** */
-
-
-
-
-
-
-
-
-
-
-
-//   //******************************** */
-//   resetPassword: async function (req, res, next) {
-//     const email = req.body.email;
-//     console.log("email", email);
-
-//     await User.findOne({ email: email }).exec((err, userByemail) => {
-//       if (err) {
-//         res.status(500).json({
-//           message: err.message,
-//           status: 500,
-//         });
-//       } else {
-//         res.status(200).json({
-//           status: 200,
-//           message: "utilisateur séléctonné par email !",
-//           data: userByemail,
-//         });
-
-//         const newToken = randtoken.generate(20);
-//         // console.log("newtoken",newToken);
-//         const sent = sendEmail(email, newToken);
-
-//         if (sent != '0') {
-
-//           const data = {
-//             token: newToken
-//           }
-//           User.findOneAndUpdate(email, data)
-
-//         }
-//       }
-
-
-
-//     });
-
-//     // await   User.findOneAndUpdate(email,newToken).exec((err,userUpdated)=>{
-//     //   if (err) {
-//     //     res.status(500).json({
-//     //       message: err.message,
-//     //       status: 500,
-//     //     });
-//     //   } else {
-
-//     //     res.status(200).json({
-//     //       status: 200,
-//     //       message: "email envoyé consuler email !",
-//     //       data: userUpdated,
-//     //     });
-//     //   }
-//     // });
-
-//   }
-// };
+  module.exports = {
+    register,
+    activationAccount,
+    login,
+    logout,
+    profile
+  }
--- Routers/RouteAuth.js
+++ Routers/RouteAuth.js
@@ -1,13 +1,17 @@
 const routeAuth = require("express").Router();
 const authController = require("../Controllers/authController");
 const resetController = require("../controllers/forgot_password.controller");
+const passport = require("passport");
 
 routeAuth.post("/login", authController.login);
 routeAuth.post("/register", authController.register);
 routeAuth.get("/account-activation/:code", authController.activationAccount);
-routeAuth.get("/logout", authController.logout);
+routeAuth.get("/logout", passport.authenticate("bearer", { session: false }), authController.logout);
+// profile
+routeAuth.get("/profile", passport.authenticate("bearer", { session: false }), authController.profile);
 // reset & fogot password
 routeAuth.post("/forgot-password", resetController.forgetPassword);
 routeAuth.post("/reset-password", resetController.resetPassword);
+
 
 module.exports = routeAuth;
